7d99f9c0b5c36f1e9fe249a893a722c22be2ed5e
Nick Evans <nick.evans@northwestern.edu>
2019-02-03 15:52:05 -0600

    Add favico

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="Movie Night event manager">
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"/>
         <meta name="csrf-token" content="{{ @csrf_token() }}">
 
         <title>Movie Night</title>
