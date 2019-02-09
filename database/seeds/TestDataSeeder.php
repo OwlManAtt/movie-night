129cef09d0d28edce3ace8cc695bd9f8c20ed280
Nick Evans <nick.evans@northwestern.edu>
2019-02-09 11:04:48 -0600

    Disable Eloquent events in seeder

--- database/seeds/TestDataSeeder.php
+++ database/seeds/TestDataSeeder.php
@@ -1,13 +1,16 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Event;
 
 class TestDataSeeder extends Seeder
 {
     public function run()
     {
+        // Disable model events during seeding
+        Event::fake();
+
         $this->call(MediaSeeder::class);
         $this->call(UserSeeder::class);
     } // end run
-
 } // end DatabaseSeeder
